7c3b535036ef857501cf35444bca9caad5f03a5a
Chase Holland <lurch09@gmail.com>
2015-10-26 20:14:26 -0500

    adding sample project and finishing renaming files

--- CAHImageCacher/Example/Pods/Target Support Files/CAHImageCacher/CAHImageCacher-umbrella.h
+++ CAHImageCacher/Example/Pods/Target Support Files/CAHImageCacher/CAHImageCacher-umbrella.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+#import "CAHImageCacher.h"
+#import "UIImage+AutoMimeType.h"
 
 FOUNDATION_EXPORT double CAHImageCacherVersionNumber;
 FOUNDATION_EXPORT const unsigned char CAHImageCacherVersionString[];
